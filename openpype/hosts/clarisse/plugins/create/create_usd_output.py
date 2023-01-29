2a80034cc930f3e11f14dcee66cbd913a82e9aea
Michael reda <eng.michaelreda@gmail.com>
2023-01-29 23:34:44 +0200

    add create usd plugin

--- openpype/hosts/clarisse/plugins/create/create_usd_output.py
+++ openpype/hosts/clarisse/plugins/create/create_usd_output.py
@@ -1,15 +1,19 @@
 import ix
 
-from openpype.pipeline.create import (
-    LegacyCreator
+from openpype.hosts.clarisse.api.exports import export_usd_options_uiobject
+from openpype.hosts.clarisse.api.lib import (
+    get_current_frame,
+    get_selected_context,
 )
+from openpype.hosts.clarisse.api.pipeline import imprint
+from openpype.pipeline import LegacyCreator
 
 
 class CreateUSDOutput(LegacyCreator):
-    """Bake a context into alembic
+    """Export a context as USD
     no shading will be exported, just geometry"""
 
-    label = "Create a USD Export - NA"
+    label = "USD export"
     family = "usd"
     icon = "magic"
     defaults = ["Main"]
@@ -17,25 +21,34 @@ class CreateUSDOutput(LegacyCreator):
     def __init__(self, *args, **kwargs):
         super(CreateUSDOutput, self).__init__(*args, **kwargs)
 
-        # Remove the active, we are checking the bypass flag of the nodes
-        self.data.pop("active", None)
-
-        # Set node type to create for output
-        self.data.update({"node_type": "usd"})
-
-    def process(self, instance):
-        """Creator main entry point.
-
-        Args:
-            instance (pyContext): selected context
-
+    def process(self):
+        """Creator main entry point of usd.
         """
+        # get selected context
+        ctx_select = get_selected_context()
+        assert ctx_select
 
-        filename = "$PDIR/pyblish/%s.usd" % self.name
+        filepath = "$PDIR/cache/usd/%s.usd" % self.name
 
-        # TODO write usd exporter
-        pass
+        # frame range
+        export_settings = {
+            "export_context": True,
+            "filename": filepath,
+            "default_prim": 1,
+            "root_prim": '',  # instance.data['root_prim'],
+            # "root_prim_name": "",
+            "standalone": 0,  # flatten_usd
+            "use_instances": 0,
+            "export_invisible_objects": 0,
+            "export_displacement": 0,
+            "export_custom_attributes": 1,
+            "custom_attributes_namespace": "",
+            "animation_mode": get_current_frame(),  # current frame
+            # "custom_frame": 0,
+            # "custom_frame_range": [instance.data['start_frame'], instance.data['end_frame']],
 
+        }
 
+        node = export_usd_options_uiobject(selected_context=ctx_select, name=self.name,  options=export_settings)
 
-
+        imprint(node, self.data, group="openpype")
